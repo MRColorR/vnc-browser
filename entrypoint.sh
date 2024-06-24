61efe666eb59e9b373b3c0bc3c9be994a21b1f04
MRColor <mattiar11@gmail.com>
2024-06-24 14:49:30 +0200

    chore: printing new infos

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Store the password
 if [ "$VNC_PASSWORD" ]; then
-    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$VNC_PASSWORD'/" /etc/supervisord.conf
+    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$VNC_PASSWORD'/" /app/conf.d/x11vnc.conf
 fi
 
 # Print current VNC info
@@ -13,6 +13,8 @@ echo "VNC Screen: ${VNC_SCREEN}"
 echo "VNC Display is: ${VNC_DISPLAY}"
 echo "VNC resolution is: ${VNC_RESOLUTION}"
 echo "VNC password is: ${VNC_PASSWORD}"
+echo "VNC Port: ${VNC_PORT}"
+echo "NOVNC Websockify Port: ${NOVNC_WEBSOCKIFY_PORT}"
 
 # Print other container's info: like     DEBIAN_FRONTEND=${DEF_DEBIAN_FRONTEND} \    LANG=${DEF_LANG} \    LANGUAGE=${DEF_LANGUAGE} \    LC_ALL=${DEF_LC_ALL}
 echo "-----------------"
@@ -21,7 +23,7 @@ echo "-----------------"
 echo "Debian Frontend: ${DEF_DEBIAN_FRONTEND}"
 echo "Lang: ${DEF_LANG}"
 echo "LC All: ${DEF_LC_ALL}"
-echo "Default website URL: ${STARTING_WEBSITE_URL}"
+echo "Homepage website URL: ${STARTING_WEBSITE_URL}"
 echo "-----------------"
 
 # Start Supervisor
