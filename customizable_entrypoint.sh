e8fb2134da6da845cc1850414ffff0e1444c68d6
MRColor <mattiar11@gmail.com>
2024-06-25 15:32:36 +0200

    feat: adding custom entrypoint logic for easier customization

--- customizable_entrypoint.sh
+++ customizable_entrypoint.sh
@@ -3,6 +3,30 @@ set -ex
 
 echo "Ready to start"
 
+# Function to run scripts
+run_script() {
+    local script=$1
+    case "$script" in
+        *.sh)
+            if [ -x "$script" ]; then
+                "$script"
+            else
+                bash "$script"
+            fi
+            ;;
+        *.py)
+            if [ -x "$script" ]; then
+                "$script"
+            else
+                python3 "$script"
+            fi
+            ;;
+        *)
+            echo "Unknown file type: $script" >&2
+            ;;
+    esac
+}
+
 if [ "$CUSTOMIZE" = "true" ]; then
     echo "Customization is enabled."
     # Check if the base entry point script exists
@@ -12,18 +36,32 @@ if [ "$CUSTOMIZE" = "true" ]; then
         echo "Base entry point is running."
         # Wait for 10 seconds
         sleep 10
+    else
+        echo "Base entry point script not found: /usr/local/bin/base_entrypoint.sh" >&2
+        exit 1
     fi
-    echo "Customization can be added here. If no additional services are defined then the container will exit."
-    echo "Running the custom entry point..."
 
-    # Custom logic or services can be added here
-    # Example: Start a Python script
-    # echo "Starting the Python script..."
-    # python3 /app/your_python_script.py
+    # Run all bash or Python scripts found inside custom entrypoints folder
+    if [ -d "$CUSTOM_ENTRYPOINTS_DIR" ]; then
+        for script in "$CUSTOM_ENTRYPOINTS_DIR"/*.{sh,py}; do
+            if [ -f "$script" ]; then
+                echo "Running custom entry point script: $script"
+                run_script "$script"
+            fi
+        done
+    else
+        echo "Custom entrypoints directory not found: $CUSTOM_ENTRYPOINTS_DIR" >&2
+        exit 1
+    fi
 
-    #### ADD YOUR CUSTOM CODE HERE ####
+    echo "Customization completed. If no additional services are defined then the container will exit."
 
 else
     echo "Customization is disabled. Running the base entry point in the foreground..."
-    exec /usr/local/bin/base_entrypoint.sh
+    if [ -f /usr/local/bin/base_entrypoint.sh ]; then
+        exec /usr/local/bin/base_entrypoint.sh
+    else
+        echo "Base entry point script not found: /usr/local/bin/base_entrypoint.sh" >&2
+        exit 1
+    fi
 fi
